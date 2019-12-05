4d5d46f1cb9bf3256f255a3e435ef6fcd993d75a
JulianFink01 <julianfink01@gmail.com>
2019-12-05 14:24:07 +0100

    Update folder

--- loginRokka/login_tpl.php
+++
@@ -1,5 +0,0 @@
-<form action="login.php" method="get">
-    <input type="text" name="uname"value="Username"required/>
-      <input type="password" name="pass"value="Password"required/>
-        <input type="submit" value="Submit"/>
-</form>
